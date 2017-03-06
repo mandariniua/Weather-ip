948a00dcc6badaa0d9ce0067a8fdc3883429e4f9
mandariniua <mandarini.ua@gmail.com>
2017-03-06 11:07:28 +0200

    'Commit'

--- Backend/ip.js
+++ Backend/ip.js
@@ -6,72 +6,79 @@ var path = require('path');
 var request = require('request');
 var rp = require('request-promise');
 
-function getIpInfo(ip, callback) {
-    var url = 'http://ip-api.com/json/' + ip;
-    request(url, function (error, response, body) {
-        if(error){
-            return callback(error)
-        }
-
-        var obj = JSON.parse(body);
-        callback(null, obj);
-        return;
-    });
-}
-
-function  getWeather(city, countryCode, callback) {
-    var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city +','
-        + countryCode + '&appid=e5131bba754d38c18d6ac1f4cffb19ac';
-    request(url, function (error, response, body) {
-        if(error){
-            return callback(error);
-        }
-        var obj = JSON.parse(body);
-        callback(null,obj);
-        return;
-    });
-}
-
-function getWeatherInfo(ip,err) {
-    if(err){
-        return err;
-    }
-    getIpInfo(ip,function (err,contents) {
-        if(err){
-            return err;
-        }
-        getWeather(contents.city,contents.countryCode,function (err,cont) {
-            if(err){
-                return err;
-            }
-            return {city:contents.city,temp:(cont.main.temp-273.15+'°C')};
-        })
-
-    });
-}
-
-function ipWeather(req, res) {
+function getIpInfo() {
     var options = {
-        method : 'POST',
-        uri: 'http://localhost/ipWeather/${ip}',
-        body: {
-            some: 'payload'
+        uri: 'http://ip-api.com/json/',
+        qs: {
+            access_token: 'http://ip-api.com/json/' + 'ip' // -> uri + '?access_token=xxxxx%20xxxxx'
+        },
+        headers: {
+            'User-Agent': 'Request-Promise'
         },
         json: true
-
     };
-
-    var response = request.get(options);
-
     rp(options)
-        .then(function getWeatherInfo(content) {
-           return content;
-
+        .then(function (ip) {
+            console.log('User has %d repos', ip.length);
         })
-            .catch(function (err) {
-                console.error('My error:',err);
-            });
+        .catch (function (err) {
+            return err;
+        });
 }
+
+// function  getWeather(city, countryCode, callback) {
+//     var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city +','
+//         + countryCode + '&appid=e5131bba754d38c18d6ac1f4cffb19ac';
+//     request(url, function (error, response, body) {
+//         if(error){
+//             return callback(error);
+//         }
+//         var obj = JSON.parse(body);
+//         callback(null,obj);
+//         return;
+//     });
+// }
+//
+// function getWeatherInfo(ip,err) {
+//     if(err){
+//         return err;
+//     }
+//     getIpInfo(ip,function (err,contents) {
+//         if(err){
+//             return err;
+//         }
+//         getWeather(contents.city,contents.countryCode,function (err,cont) {
+//             if(err){
+//                 return err;
+//             }
+//             return {city:contents.city,temp:(cont.main.temp-273.15+'°C')};
+//         })
+//
+//     });
+// }
+
+// function ipWeather() {
+//     var options = {
+//         method : 'POST',
+//         uri: 'http://localhost/ipWeather/${ip}',
+//         body: {
+//             some: 'payload'
+//         },
+//         json: true
+//
+//     };
+//
+//     var response = request.get(options);
+//
+//     rp(options)
+//         .then(function getWeatherInfo(content) {
+//            return content;
+//
+//         })
+//             .catch(function (err) {
+//                 console.error('My error:',err);
+//             });
+// }
 
 // getWeatherInfo('62.216.46.98',function (err,contents) {
 //     console.log(err,contents);
@@ -88,4 +95,4 @@ client.sshkeys.create({
     // do something with the key
 });
 
-exports.process = ipWeather;
+exports.process = getIpInfo;
